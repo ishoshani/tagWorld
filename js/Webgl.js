7b2c01f4b9b26e340030314f279da9861cd7314a
isho <idoshoshani@gmail.com>
2016-11-12 20:02:26 -0800

    skins

--- js/Webgl.js
+++ js/Webgl.js
@@ -103,6 +103,8 @@ scene.add( tag1 );
 scene.add( tag2) ;
 scene.add(tag3);
 scene.add(tag4);
+var pointLight = new THREE.PointLight(0xFFFFFF);
+scene.addLight(pointLight);
 var lastOrientation
 window.ondeviceorientation = function(event){
 	if(!initialized){
