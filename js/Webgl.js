5f14a12016625f8431f31080511949bceb74f8d4
isho <idoshoshani@gmail.com>
2016-11-13 11:46:05 -0800

    onclick

--- js/Webgl.js
+++ js/Webgl.js
@@ -92,7 +92,7 @@ backscene.add(backCamera);
 var raycaster = new THREE.Raycaster(); // create once
 var mouseVector = new THREE.Vector3() // create once
 
-function onMouseMove(){
+function onMouseMove(e){
 	mouseVector.x = 2 * (e.clientX / containerWidth) - 1;
 	mouseVector.y = 1 - 2 * ( e.clientY / containerHeight );
 	var raycaster = projector.pickingRay( mouseVector.clone(), camera );
