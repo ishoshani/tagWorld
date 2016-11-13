7760e2ef55e96728323516f201ded6124f2d48c3
isho <idoshoshani@gmail.com>
2016-11-13 11:51:07 -0800

    onclick

--- js/Webgl.js
+++ js/Webgl.js
@@ -95,7 +95,7 @@ var mouseVector = new THREE.Vector3() // create once
 function onMouseMove(e){
 	mouseVector.x = 2 * (e.clientX / renderer.domElement.clientWidth) - 1;
 	mouseVector.y = 1 - 2 * ( e.clientY / renderer.domElement.clientWidth );
-	raycaster.setFromCamera( mouse, camera );
+	raycaster.setFromCamera( mouseVector, camera );
 	var intersects = raycaster.intersectObjects( scene.children );
 	console.log(intersects[0]);
 }
